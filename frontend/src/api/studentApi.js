f1be288f18a8c7f7832e11777b915b97023d58c3
nancymish <143929737+nancymish@users.noreply.github.com>
2025-12-23 11:59:20 +0530

    Fix axios  deployment

--- frontend/src/api/studentApi.js
+++ frontend/src/api/studentApi.js
@@ -1,5 +1,11 @@
 import API from "./axios";
 
+const API = axios.create({
+  baseURL: "https://student-management-system-wf67.onrender.com/api",
+  withCredentials: true,
+});
+
+
 export const getProfile = () => API.get("/profile");
 
 export const updateProfile = (data) =>
